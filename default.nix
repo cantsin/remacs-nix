a323cc5077bc7082c6c1a06e0c3a485b817b2abd
James Tranovich <jtranovich@gmail.com>
2019-06-22 23:36:20 -0700

    add README.md

--- default.nix
+++ default.nix
@@ -1,16 +1,20 @@
 with import <nixpkgs> {};
 
-let src = fetchFromGitHub {
-      owner = "mozilla";
-      repo = "nixpkgs-mozilla";
-      rev = "50bae918794d3c283aeb335b209efd71e75e3954";
-      sha256 = "07b7hgq5awhddcii88y43d38lncqq9c8b2px4p93r5l7z0phv89d";
-      # date = 2019-04-02T09:39:52+02:00;
-   };
+let
+  src = fetchFromGitHub {
+    owner = "mozilla";
+    repo = "nixpkgs-mozilla";
+    rev = "50bae918794d3c283aeb335b209efd71e75e3954";
+    sha256 = "07b7hgq5awhddcii88y43d38lncqq9c8b2px4p93r5l7z0phv89d";
+    # date = 2019-04-02T09:39:52+02:00;
+  };
 in
+
 with import "${src.out}/rust-overlay.nix" pkgs pkgs;
-# date here must mirror remacs/rust-toolchain
-let rustWithSource = ((rustChannelOf { date = "2019-05-01"; channel = "nightly"; }).rust.override {
+
+let
+  # as per remacs/rust-toolchain
+  rustWithExtensions = ((rustChannelOf { date = "2019-05-01"; channel = "nightly"; }).rust.override {
     extensions = [
       "rust-src"
       "clippy-preview"
@@ -19,13 +23,15 @@ let rustWithSource = ((rustChannelOf { date = "2019-05-01"; channel = "nightly";
     ];
   });
 in
-stdenv.mkDerivation rec {
-  version = "27.0.1";
 
-  name = "remacs-${version}";
+stdenv.mkDerivation rec {
+  name = "remacs-${version}${versionModifier}";
+  # as per remacs/configure.ac AC_INIT
+  version = "27.0.50";
+  versionModifier = "-git";
 
   buildInputs = [
-    rustWithSource rustfmt rustracer ctags
+    rustWithExtensions rustfmt rustracer ctags
 
     systemd texinfo libjpeg libtiff giflib xorg.libXpm gtk3 gnutls
     ncurses libxml2 xorg.libXt imagemagick librsvg gpm dbus libotf
